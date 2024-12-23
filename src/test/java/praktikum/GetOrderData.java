c06109fef724ac0670de347d0a42ebcd2fca0908
TT <iloewen79@gmx.de>
2024-12-23 17:04:01 +0100

    Создал тестовую среду для GetOrderData

---
+++ src/test/java/praktikum/GetOrderData.java
@@ -0,0 +1,42 @@
+package praktikum;
+
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import steps.UserSteps;
+
+import static data.Data.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class GetOrderData {
+
+    public UserSteps userSteps;
+    public Response response;
+    public String token;
+
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://stellarburgers.nomoreparties.site";
+        userSteps = new UserSteps();
+        response = userSteps.createUser(VALID_UNIQUE_USER_REQUEST_BODY);
+        userSteps.getAccessToken(response);
+        token = userSteps.accessToken;
+    }
+
+@Test
+
+    @After
+    public void tearDown() {
+        if (response != null) {
+            userSteps.getAccessToken(response);
+            userSteps.deleteUser();
+        }
+        response = null;
+        token = null;
+    }
+}
