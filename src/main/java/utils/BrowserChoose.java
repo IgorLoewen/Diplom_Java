a384f32ec8aeafccdf2a68f8daff21d581a6daca
TT <iloewen79@gmx.de>
2024-12-24 12:31:47 +0100

    Настроил браузеры и опции по максимальному возможно нужному стандарту

--- src/main/java/utils/BrowserChoose.java
+++ src/main/java/utils/BrowserChoose.java
@@ -7,30 +7,41 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class BrowserChoose {
 
     public static WebDriver createDriver(String browser) {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver"); // Укажите корректный путь к драйверу
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        addCommonArguments(options); // Общие аргументы для всех браузеров
 
         if (browser.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
+            // Специфичные настройки для Chrome
             options.addArguments("--disable-features=FederatedCredentialManagement");
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--disable-gpu");
-            return new ChromeDriver(options);
 
         } else if (browser.equalsIgnoreCase("yandex")) {
-            ChromeOptions options = new ChromeOptions();
-            options.setBinary("/Applications/Yandex.app/Contents/MacOS/Yandex"); // Укажите путь к Yandex Browser
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--disable-blink-features=AutomationControlled");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--start-maximized");
-            options.addArguments("--enable-automation");
-            return new ChromeDriver(options);
+            // Специфичные настройки для Yandex
+            options.setBinary("/Applications/Yandex.app/Contents/MacOS/Yandex");
 
         } else {
             throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
+
+        return new ChromeDriver(options);
+    }
+
+    // Общие аргументы для всех браузеров
+    private static void addCommonArguments(ChromeOptions options) {
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--start-maximized");
+        options.addArguments("--enable-automation");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-background-networking");
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-autofill-keyboard-accessory-view[8]");
     }
 }
